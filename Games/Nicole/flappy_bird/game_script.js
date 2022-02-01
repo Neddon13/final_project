a867ff3908a2df3dd4d0cf35f9197b7ddf81a67c
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2022-02-01 15:19:00 +0000

    changed the structure

--- Games/Nicole/flappy_bird/game_script.js
+++ Games/Nicole/flappy_bird/game_script.js
@@ -1,35 +1,18 @@
-const pipes = document.getElementById("pipes");
-const gap = document.getElementById("gap");
-const bird = document.getElementById("bird");
-const jumping = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game')
+    const ground = document.querySelector('.ground')
 
-gap.addEventListener('animationiteration', () => {
-    const random = -((Math.random()*300)+150);
-    gap.style.top = random + "px"
-});
+    let birdLeft = 270
+    let birdBottom = 150
+    let gravity = 3
 
-setInterval(function(){
-    const birdTop = 
-    parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
-    if(jumping==0){
-    bird.style.top = (birdTop+3)+"px";
+
+    function startGame(){
+        birdBottom -= gravity
+        bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
     }
-},10);
+    let timerId = setInterval(startGame, 20)
 
-function jump(){
-    jumping = 1;
-    let jumpCount = 0;
-    const jumpInterval = setInterval(function(){
-        const birdTop = 
-        parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
-        if((birdTop>6)&&(counter<15)){
-        bird.style.top = (birdTop-5)+"px";
-        }
-        if(jumpCount>20){
-            clearInterval(jumpInterval);
-            jumping = 0;
-            jumpCount = 0;
-        }
-        jumpCount++;
-    }, 10);
-}
+})
