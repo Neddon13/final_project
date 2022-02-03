dd6b29745a17e19b8e42d077498acdd9eb585871
Louis Fletcher <Louisco1999@gmail.com>
2022-02-03 15:24:15 +0000

    added all boiler plate for html and game file now added in sprites for map creation

---
+++ Games/2d_speed_run_blaster/game.js
@@ -0,0 +1,51 @@
+//setup for Kaboom game library and is importated through html file
+kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 3,
+    debug: true,
+    clearColor: [0, 0, 1, 1],
+  })
+
+// player, enemey and boss speed 
+  const playerSpeed = 130
+  const enemeySpeed = 100
+  const bossSpeed = 150
+
+
+  //laoding in sprites for game characters and objects for mapping levels though imgur account
+  loadRoot('https://i.imgur.com/')
+  loadSprite('bottom-wall', 'GGgrnFh.png')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
+  loadSprite('')
