61b512af0a952fa538262e0a8468b53bd5146d14
Louis Fletcher <Louisco1999@gmail.com>
2022-02-03 15:25:39 +0000

    added function to start game

--- Games/2d_speed_run_blaster/game.js
+++ Games/2d_speed_run_blaster/game.js
@@ -15,37 +15,38 @@ kaboom({
 
   //laoding in sprites for game characters and objects for mapping levels though imgur account
   loadRoot('https://i.imgur.com/')
-  loadSprite('bottom-wall', 'GGgrnFh.png')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
-  loadSprite('')
+  loadSprite('bottom-wall', 'GGgrnFh.png')  // S
+  loadSprite('right-wall-Y-flag','blvTV9B.png') //S
+  loadSprite('right-wall-R-flag', '0M3KY1J.png')    //S
+  loadSprite('right-wall-G-flag', '88Hky91.png')    //S
+  loadSprite('right-wall-B-flag', 'tcdwQOu.png')    //S
+  loadSprite('pillar-top', 'TEYuPFZ.png')   //S
+  loadSprite('pillar-base', 'ih1tmYd.png')  //S
+  loadSprite('pillar-body', 'PKYGY5T.png')  //S
+  loadSprite('top-wall', 'PatuSh9.png') //S
+  loadSprite('right-wall', '17t8kMm.png') //S
+  loadSprite('top-right-corner', 'TOOO8Ma.png') //S
+  loadSprite('top-left-corner', 'b2P8Uam.png')  //S
+  loadSprite('stairs-right', '77BWeTa.png') //NS
+  loadSprite('bottom-right-next-level-door', 'tWsoh5t.png') //NS
+  loadSprite('bottom-left-next-level-door', 'nrN8NHD.png')  //NS
+  loadSprite('ladders-up-or-down-right-side', 'yrcUwIk.png') //NS
+  loadSprite('left-wall', 'eletAMJ.png') // S
+  loadSprite('ladders-up', 'QFTJuE9.png') // NS
+  loadSprite('ladders-up-exit-hole', 'KR0DMJJ.png') //S
+  loadSprite('gunge-flow-bottom', 'VKaIwiT.png')// NS
+  loadSprite('bottom-right-corner', 'xzIPIsh.png') //S
+  loadSprite('bottom-left-corner', 'JDrxVjq.png') //S
+  loadSprite('water-flow-top', 'yp2IeuT.png') //S
+  loadSprite('water-flow-bottom', 'E4I8hh3.png') //NS
+  loadSprite('shadow', 'DhrVZ0n.png') //S
+  loadSprite('left-wall-Y-flag', 'rYfz2wm.png') //S
+  loadSprite('left-wall-G-flag', 'm5ufziB.png') //S
+  loadSprite('left-wall-R-flag', 'eHx4ppW.png') //S
+  loadSprite('left-wall-B-flag', 'SIhBYWy.png') //S
+  loadSprite('lava-flow-top', '5TW0ktX.png') //S
+  loadSprite('lava-flow-bottom', '3DXREXE.png') //NS
+  loadSprite('gunge-flow-top', 'Od2ZfHj.png') //S
+  loadSprite('bg', '62HrIvb.png') //NS
+  
+start("game")
