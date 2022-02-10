8c13697bc19342bf78bc685a9a65a1543291b7c7
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2022-02-10 11:58:04 +0000

    adding more css

--- client/src/components/PageHeader.js
+++ client/src/components/PageHeader.js
@@ -1,20 +1,15 @@
-import logo from '../images/logo.png';
 import Payment from '../components/Payment';
 
 const PageHeader = () => {
     return (
         <>
         <header>
-            <div id="top-bar">
-                <button Login >Login</button>
-            </div>
             <div id="main-header" className="flex-row">
                 <div id="page-logo"> 
-                <img src={logo} width="70vw" height="70vh"></img>
+                <h1>Game World</h1>
                 </div>
-            <nav id="nav-bar">Nav bar</nav>
+                <hr></hr>
             <Payment/>
-            
             </div>
         </header>
         </>
